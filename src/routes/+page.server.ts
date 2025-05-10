fcb3de14b88a94e6e7db576508806897bd5c8866
Cameron Johnson <cajoro935@gmail.com>
2025-05-10 07:12:54 -0600

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,15 +4,11 @@ import type { PageServerLoad } from './$types'
 // This function loads all of the rows from the todo table
 // and returns them to the page.
 export const load: PageServerLoad = async () => {
-  const { data, error } = await supabase
+  const { data } = await supabase
     .from('todo')
     .select('id, name, goal, complete_by')
     // We use order so the most present tasks are first.
     .order('complete_by', { ascending: true })
-
-  if (error) {
-    return { todos: [] }
-  }
 
   return { todos: data }
 }
