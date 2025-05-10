3ebf9dab7ecd7b5546d9da0a3fd1f5c1f12a7998
Cameron Johnson <cajoro935@gmail.com>
2025-05-10 07:50:16 -0600

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,21 +1,14 @@
 import { supabase } from '$lib/supabaseClient'
-import type { PageServerLoad } from './$types'
 
 // This function loads all of the rows from the todo table
 // and returns them to the page.
-
-export const load: PageServerLoad = async () => {
-  const { data, error } = await supabase
-    .from('todo')
-    .select('id, name, goal, complete_by')
-    .order('complete_by', { ascending: true });
+export async function load() {
+  const { data, error } = await supabase.from("todo").select();
 
   if (error) {
-    console.error('Error loading todos:', error.message);
+    alert(error);
     return { todos: [] };
   }
 
-  return {
-    todos: data ?? []
-  };
-};
+  return { todos: data ?? [] };
+}
