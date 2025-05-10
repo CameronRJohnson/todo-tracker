ef85ee603149c7fe7c22cc305bb950f08cc89bbc
Cameron Johnson <cajoro935@gmail.com>
2025-05-10 02:41:59 -0600

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+// This should normally be in a .env file
+// Since our Repository is public, there is no need.
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient('https://naazzqedfiicelhhljdd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hYXp6cWVkZmlpY2VsaGhsamRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4NDE3NTksImV4cCI6MjA2MjQxNzc1OX0.wQB6l6FQh--KQtOFC0t70CPb4oAYSl4oShJJ0DIwvmk')
